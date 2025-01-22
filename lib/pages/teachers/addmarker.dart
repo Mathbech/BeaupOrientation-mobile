c2908bcb1005335387eaf72c5e0801f43ac3a682
Mathbech <legeek.you@gmail.com>
2025-01-22 09:06:39 +0100

    Refactor AddMarkerPage to use MarkerAddService for location and address retrieval

--- lib/pages/teachers/addmarker.dart
+++ lib/pages/teachers/addmarker.dart
@@ -1,8 +1,8 @@
-import 'package:beauporientation/widgets/custom_app_bar.dart';
-import 'package:beauporientation/widgets/custom_drawer.dart';
-import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:geocoding/geocoding.dart';
+import '../../widgets/custom_app_bar.dart';
+import '../../widgets/custom_drawer.dart';
+import 'package:flutter/material.dart';
+import '../../services/marker_add_service.dart';
 
 class AddMarkerPage extends StatefulWidget {
   const AddMarkerPage({super.key});
@@ -13,50 +13,15 @@ class AddMarkerPage extends StatefulWidget {
 
 class _AddMarkerPageState extends State<AddMarkerPage> {
   final List<String> _markers = [];
+  final MarkerAddService _markerAddService = MarkerAddService();
 
   Future<void> _addMarker() async {
-    Position position = await _determinePosition();
-    String address = await _getAddressFromLatLng(position);
+    Position position = await _markerAddService.determinePosition();
+    String address = await _markerAddService.getAddressFromLatLng(position);
 
     setState(() {
       _markers.add('Balise ${_markers.length + 1}: $address');
     });
-  }
-
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    return await Geolocator.getCurrentPosition();
-  }
-
-  Future<String> _getAddressFromLatLng(Position position) async {
-    try {
-      List<Placemark> placemarks = await placemarkFromCoordinates(
-          position.latitude, position.longitude);
-      Placemark place = placemarks[0];
-      return '${place.street}, ${place.locality}, ${place.country}';
-    } catch (e) {
-      return 'Adresse non trouvée';
-    }
   }
 
   @override
@@ -65,12 +30,13 @@ class _AddMarkerPageState extends State<AddMarkerPage> {
       appBar: CustomAppBar(title: 'Balisage', showMenuButton: true),
       drawer: CustomDrawer(),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Ajouter une balise', style: TextStyle(fontSize: 18)),
-            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _addMarker,
+              child: Text('Ajouter un marqueur'),
+            ),
             Expanded(
               child: ListView.builder(
                 itemCount: _markers.length,
@@ -83,10 +49,6 @@ class _AddMarkerPageState extends State<AddMarkerPage> {
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addMarker,
-        child: Icon(Icons.add),
       ),
     );
   }
